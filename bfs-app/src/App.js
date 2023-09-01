bf43bd77d4d693d148a8ff55aed4414085e5c7de
abhu11 <mrabhishekvishvakarma@gmail.com>
2023-09-01 10:14:53 +0000

    final

--- bfs-app/src/App.js
+++ bfs-app/src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-//import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
